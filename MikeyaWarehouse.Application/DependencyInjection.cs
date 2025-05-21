54a3cb097b1b78f3e50cda903caf7b417001c30f
usr <usr@PC1>
2025-05-21 10:38:01 +0300

    Adds Validations And Register All Application Layer Dependencies

---
+++ MikeyaWarehouse.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MikeyaWarehouse.Application.Common.Behaviors;
+
+namespace MikeyaWarehouse.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(
+            cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddTransient(
+            typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(
+            Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
